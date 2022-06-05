804dbd60965a0d0b76f06e1be7b690fdc0f349c1
mark hancock <mwhancock2@gmail.com>
2022-06-05 01:55:30 -0230

    start adding js functionality, refactor html and css

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const display = document.getElementById('disp-txt');
+const buttons = document.querySelectorAll('button');
+
 function add(n1, n2){
   return n1 + n2;
 }
 
-function subtact(n1, m2){
+function subtract(n1, n2){
   return n1 - n2;
 }
 
@@ -15,20 +18,46 @@ function divide(n1, n2){
 }
 
 function operate(operator, n1, n2){
-  let result = 0;
+  let result;
   switch(operator){
     case '+':
-      result += add(n1, n2);
+      result = add(n1, n2);
       break;
-    case '-':
-      result += subtract(n1, n2);
+    case '−':
+      result = subtract(n1, n2);
       break;
-    case '*':
-      result += multiply(n1, n2);
+    case '×':
+      result = multiply(n1, n2);
       break;
-    case '/':
-      result += divide(n1, n2);
+    case '÷':
+      result = divide(n1, n2);
       break;
   };
-  return Math.round((result + Number.EPSILON) * 100) /100;
+  return Number.parseFloat(result.toFixed(3));
 };
+
+
+let num1;
+let num2;
+let op;
+function calculate(operator, n1, n2){
+  buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      if(e.target.innerText === '='){
+        display.innerText = operate(op, num1, num2);
+        console.log(operate(op, num1, num2))
+      }else if(e.target.className === 'op-btn'){
+        op = e.target.innerText;
+        display.innerText += ' ' + op;
+      }else if(!num1){
+        num1 = parseInt(e.target.innerText);
+        display.innerText = e.target.innerText;
+      }else if(num1 && !num2){
+        num2 = parseInt(e.target.innerText);
+        display.innerText += ' ' + e.target.innerText;
+      }
+    })
+  })
+}
+
+calculate(op, num1, num2);
