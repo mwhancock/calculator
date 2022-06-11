181312811f0f77fbac4b6073313c35ac1bf2df2f
mark hancock <mwhancock2@gmail.com>
2022-06-11 08:27:40 -0230

    refactor code

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const display = document.getElementById('disp-txt');
-const buttons = document.querySelectorAll('button');
+const clear = document.getElementById('clear');
+const del = document.querySelector('delete');
 const numButtons = Array.from(document.getElementsByClassName('num-btn'));
 const opButtons = Array.from(document.getElementsByClassName('op-btn'));
 const eql = document.getElementById('equal');
+let num1;
+let num2;
+let op;
+
 
 function add(n1, n2){
   return n1 + n2;
@@ -39,39 +44,62 @@ function operate(operator, n1, n2){
   return Number.parseFloat(result.toFixed(3));
 };
 
-
-function calculate(operator, n1, n2){
-  buttons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      // display.innerText = operate(operator, n1, n2);
-    })
-  })
+function getNum1(num){
+  if(!op){
+    display.innerText += num;
+    num1 = display.innerText;
+    console.log(num1)
+  }
 }
 
-let num1;
-let num2;
- function getNum1(){
-  numButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      let num;
-      display.innerText += e.target.innerText;
-      if(e !== numButtons){
-        num1 = display.innerText;
-        if(num1 && e !== eql){
-          display.innerText += display.innerText;
-        }
-      }
-    })
-  })
+function getNum2(num){
+  if(op){
+    clearDisp();
+    display.innerText += num;
+    num2 = display.innerText;
+    console.log(num2)
+  }
 }
 
-function getOperator(){
-  opButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      let op = e.target.innerText;
-      display.innerText += op;
-
-    })
-  })
+function getOperator(operator){
+  op = operator;
+  display.innerText = op;
 }
-calculate(getOperator(), num1, num2;
+
+function clearCalc(){
+    num1 = undefined;
+    num2 = undefined;
+    op = undefined;
+    display.innerText = '';
+}
+
+function clearDisp(){
+  clearDisp = function(){};
+  display.innerText = '';
+}
+
+
+
+numButtons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    getNum1(e.target.innerText);
+    getNum2(e.target.innerText);
+  })
+})
+
+opButtons.forEach(btn => {
+  btn.addEventListener('click', e => {
+    getOperator(e.target.innerText);
+  })
+})
+
+clear.addEventListener('click', () => {
+  clearCalc();
+})
+
+eql.addEventListener('click', () => {
+  display.innerText = operate(op, num1, num2);
+  num1 = num2;
+  num2 = undefined;
+  op = undefined;
+})
